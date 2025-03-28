e423dbde4803fa1074b524b1cf68f3219f5016b9
Lars <lars.veljaca1@icloud.com>
2025-03-28 14:36:52 +0100

    Aufgabe done

---
+++ EFCoreApplication/EFCoreApplication/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreApplication
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=people.db");
+        }
+    }
+
+}
---
+++ EFCoreApplication/EFCoreApplication/Program.cs
@@ -0,0 +1,46 @@
+﻿// Program.cs
+using EFCoreApplication;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new DBContext())
+        {
+            context.Database.EnsureCreated();
+
+            var person = new Person
+            {
+                FirstName = "Lars",
+                LastName = "Veljaca",
+                Age = 17
+
+             
+            };
+
+            var person2 = new Person
+            {
+                FirstName = "Hans",
+                LastName = "Peter",
+                Age = 55
+            };
+
+            context.People.Add(person);
+            context.People.Add(person2);
+            context.SaveChanges();
+
+            var people = context.People.ToList();
+
+            Console.WriteLine("Alle Personen in der Datenbank:");
+            foreach (var p in people)
+            {
+                Console.WriteLine($"Id: {p.Id}, Name: {p.FirstName} {p.LastName}, Age: {p.Age}");
+            }
+
+            
+            
+        }
+    }
+}
